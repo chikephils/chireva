724807ae6101d89d1dab06f85cb5f47d93679698
chikephils <chikephils@gmail.com>
2024-04-06 17:38:16 +0100

    updated

--- frontend/src/components/Shop/ShopProfileData.jsx
+++ frontend/src/components/Shop/ShopProfileData.jsx
@@ -103,7 +103,7 @@ const ShopProductsTab = ({ data, isLoading }) => {
           No products available.
         </div>
       ) : (
-        <div className="grid grid-cols-2 gap-[20px] md:grid-cols-4 md:gap-[30px] lg:grid-cols-4 lg:gap-[40px] xl:grid-cols-5 xl:gap-[50px] 600px:grid-cols-3 600px:gap-[10px] pb-10 justify-items-center">
+        <div className="grid grid-cols-2 gap-[20px] md:grid-cols-4 md:gap-[30px] lg:grid-cols-4 lg:gap-[40px] xl:grid-cols-5 xl:gap-[40px] 600px:grid-cols-3 600px:gap-[30px] pb-10 justify-items-center">
           {data &&
             data.map((product) => (
               <SellerProductCard key={product._id} product={product} />
