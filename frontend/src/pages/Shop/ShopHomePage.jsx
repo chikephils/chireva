e703758ae00fbd8e512b9c0348ffb365d79d6c22
chikephils <chikephils@gmail.com>
2024-04-06 18:57:12 +0100

    updated

--- frontend/src/pages/Shop/ShopHomePage.jsx
+++ frontend/src/pages/Shop/ShopHomePage.jsx
@@ -28,28 +28,31 @@ const ShopHomePage = () => {
   }, [id]);
 
   return (
-    <div className={`  mt-2 `}>
-      <div className="w-full lg:flex  justify-between">
-        {loading ? (
-          <div className="flex items-center justify-center">
-            <Loader />
-          </div>
-        ) : (
-          <>
-            <div className="lg:w-[25%] bg-[#fff] rounded-xl shadow-2xl lg:sticky top-0 left-0 z-10 px-2 h-full py-4">
-              <div className="overflow-y-scroll scrollbar-none h-[95vh] bg-gray-300 rounded-md shadow-lg">
-                <ShopInfo isOwner={true} shopId={id} shop={shop} />
+    <>
+      {loading ? (
+        <div className="flex items-center justify-center h-[80vh]">
+          <Loader />
+        </div>
+      ) : (
+        <>
+          <div className={` bg-slate-200 lg:px-4 `}>
+            <div className="w-full lg:flex justify-between">
+              <div className="lg:w-[25%] bg-stone-200 rounded-[4px] shadow-sm  lg:sticky left-0 z-10 px-2 py-4 h-full">
+                <div className="overflow-y-scroll scrollbar-none h-[95vh] bg-slate-200 rounded-md shadow-lg">
+                  <ShopInfo isOwner={true} shopId={id} shop={shop} />
+                </div>
+              </div>
+
+              <div className="800px:w-[72%]  800px:mt-['unset'] rounded-[4px]">
+                <div className=" overflow-y-scroll scrollbar-none h-screen">
+                  <ShopProfileData isOwner={true} />
+                </div>
               </div>
             </div>
-          </>
-        )}
-        <div className="lg:w-[72%] rounded-[4px]  800px:mt-['unset']">
-          <div className=" overflow-y-scroll scrollbar-none h-screen">
-            <ShopProfileData isOwner={true} />
           </div>
-        </div>
-      </div>
-    </div>
+        </>
+      )}
+    </>
   );
 };
 
